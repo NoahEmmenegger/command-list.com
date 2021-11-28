f82f1eac7e98dba6178fc50d79233ac56c8b9ecd
Noah Emmenegger <noahemmenegger@gmx.ch>
2021-11-28 20:40:31 +0100

    [wip] dashboard

--- components/dashboard/Section.js
+++ components/dashboard/Section.js
@@ -1,26 +1,43 @@
-import EditCommand from "./Command"
-import { Draggable } from "react-beautiful-dnd"
-import { useState } from "react"
+import EditCommand from "./Command";
+import { Draggable } from "react-beautiful-dnd";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function EditSection({ section, index }) {
-    const [isHidden, setIsHidden] = useState(true)
+  const [isHidden, setIsHidden] = useState(true);
 
-    return (
-        <Draggable key={section.id} draggableId={section.id.toString()} index={index}>
-            {(provided, snapshot) => (
-                <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                >
-                    <div className="card p-10 m-10 bg-gray-50" onClick={() => setIsHidden(!isHidden)}>
-                        <input type="text" value={section.title} />
-                        {!isHidden ? section.commands.map(command => {
-                            return <EditCommand key={command.id} command={command} />
-                        }) : null}
-                    </div>
-                </div>
-            )}
-        </Draggable>
-    )
+  return (
+    <Draggable
+      key={section.id}
+      draggableId={section.id.toString()}
+      index={index}
+    >
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className="card p-10 m-10 bg-gray-50">
+            <div className="flex">
+              <div className="mr-auto">
+                <input type="text" value={section.title} />
+              </div>
+              <div>
+                <button onClick={() => setIsHidden(!isHidden)}>
+                  <Image alt="" src="/icons/arrow.svg" height="30" width="30" />
+                </button>
+              </div>
+            </div>
+            {!isHidden
+              ? section.commands.map((command) => {
+                  return <EditCommand key={command.id} command={command} />;
+                })
+              : null}
+          </div>
+        </div>
+      )}
+    </Draggable>
+  );
 }
--- pages/dashboard/[slug]/index.js
+++ pages/dashboard/[slug]/index.js
@@ -1,70 +1,87 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import DashboardLayout from '../../../components/dashboard/Layout'
-import EditSection from '../../../components/dashboard/Section'
-import Loading from '../../../components/Loading'
-import { useAuth } from '../../../utils/auth'
-import { getPageBySlug } from '../../../utils/pages'
-import Custom404 from '../../404'
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import DashboardLayout from "../../../components/dashboard/Layout";
+import EditSection from "../../../components/dashboard/Section";
+import Loading from "../../../components/Loading";
+import { useAuth } from "../../../utils/auth";
+import { getPageBySlug } from "../../../utils/pages";
+import Custom404 from "../../404";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Edit() {
-    const router = useRouter()
-    const auth = useAuth()
+  const router = useRouter();
+  const auth = useAuth();
 
-    const { slug } = router.query
+  const { slug } = router.query;
 
-    const [page, setPage] = useState(null)
+  const [page, setPage] = useState(null);
 
-    useEffect(() => {
-        const initPage = async () => {
-            setPage(await getPageBySlug(slug))
-        }
-        initPage()
-    }, [slug])
+  useEffect(() => {
+    const initPage = async () => {
+      setPage(await getPageBySlug(slug));
+    };
+    initPage();
+  }, [slug]);
 
-    if (!page || Object.keys(page).length === 0) {
-        return <DashboardLayout>
-            <Loading />
-        </DashboardLayout>
-    }
-
-    if (!auth.userId || page.ownerUid !== auth.userId) {
-        return <Custom404 />
-    }
-
-    const menuItems = [
-        {
-            image: '/icons/edit.svg',
-            title: 'Edit',
-            href: ''
-        },
-        {
-            image: '/icons/edit.svg',
-            title: 'Analythics',
-            href: ''
-        },
-    ]
-
+  if (!page || Object.keys(page).length === 0) {
     return (
-        <DashboardLayout title={page.title} menuItems={menuItems}>
-            <div className="w-full">
-                <DragDropContext onDragEnd={event => console.log(event)}>
-                    <Droppable droppableId="droppable">
-                        {(provided, snapshot) => (
-                            <div
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                            >
-                                {page.sections.map((section, index) => (
-                                    <EditSection key={section.id} section={section} index={index} />
-                                ))}
-                                {provided.placeholder}
-                            </div>
-                        )}
-                    </Droppable>
-                </DragDropContext>
-            </div>
-        </DashboardLayout>
-    )
+      <DashboardLayout>
+        <Loading />
+      </DashboardLayout>
+    );
+  }
+
+  if (!auth.userId || page.ownerUid !== auth.userId) {
+    return <Custom404 />;
+  }
+
+  const menuItems = [
+    {
+      image: "/icons/edit.svg",
+      title: "Edit",
+      href: "",
+    },
+    {
+      image: "/icons/edit.svg",
+      title: "Analythics",
+      href: "",
+    },
+  ];
+
+  return (
+    <DashboardLayout title={page.title} menuItems={menuItems}>
+      <Link href={`/page/${page.title.toLowerCase()}`}>
+        <a target="_blank" className="btn flex m-auto mr-0 mb-4">
+          <p className="m-auto mr-3">view</p>
+          <Image alt="" src="/icons/link.svg" height="30" width="30" />
+        </a>
+      </Link>
+      <div className="w-full">
+        <DragDropContext onDragEnd={(event) => console.log(event)}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {page.sections.map((section, index) => (
+                  <EditSection
+                    key={section.id}
+                    section={section}
+                    index={index}
+                  />
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+      <div>
+        <button className="btn flex m-auto mr-0 mb-4">
+          <p className="m-auto mr-3">Add section</p>
+          <Image alt="" src="/icons/plus.svg" height="30" width="30" />
+        </button>
+      </div>
+    </DashboardLayout>
+  );
 }
