ec7a3e2f044fc452e0a859dd197e4535c831050f
Noah Emmenegger <noahemmenegger@gmx.ch>
2022-01-09 13:00:27 +0100

    Add oAuth styling

--- components/Auth.js
+++ components/Auth.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Auth({ onclick, title, error }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     return (
-        <div className="flex flex-col px-5 max-w-2xl mx-auto">
+        <div className="flex flex-col px-5 max-w-2xl mx-auto pt-10">
             <h1>{title}</h1>
             <div className="my-5">
                 <input
@@ -35,6 +36,32 @@ export default function Auth({ onclick, title, error }) {
                     onclick({ email, password });
                 }}
             />
+            <div className="mt-20">
+                <button className="provider">
+                    <div className="p-4">
+                        <div className="h-7 w-7 relative p-3">
+                            <Image
+                                alt="google logo"
+                                src="/icons/oAuth/google.svg"
+                                layout="fill"
+                            />
+                        </div>
+                    </div>
+                    <span className="my-auto">Sign in with Google</span>
+                </button>
+                <button className="provider">
+                    <div className="p-4">
+                        <div className="h-7 w-7 relative p-3">
+                            <Image
+                                alt="github logo"
+                                src="/icons/oAuth/github.svg"
+                                layout="fill"
+                            />
+                        </div>
+                    </div>
+                    <span className="my-auto">Sign in with GitHub</span>
+                </button>
+            </div>
         </div>
     );
 }
